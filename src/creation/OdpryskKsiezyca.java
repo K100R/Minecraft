97b0cc932d1c860ceb6e8fc48dea3662d5ccf87d
PI100 <masterkastor@gmail.com>
2017-10-10 16:13:30 +0200

    Planowany kreator Odprysku Księżyca

---
+++ src/creation/OdpryskKsiezyca.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package creation;
+
+/**
+ *
+ * @author ppacholski
+ */
+public class OdpryskKsiezyca {
+    
+    Dwie piramidy o tej samej podstawie, dolna niska, górna wysoka
+            Rozkminić orientację schodów
+    
+}
